2ca5ac32c3853a60034ac95a9798d5cf9bb6b551
thehuseyinoglu <thehuseyinoglu@gmail.com>
2023-01-19 22:29:49 +0300

    import component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { ref, reactive } from "vue"
+import Room from './components/Room.vue'
+import Chat from './components/Chat.vue'
+
+const state = reactive({
+  quickChat: {
+    userName: '',
+    room: '',
+    chatScreen: false
+  },
+})
+
+</script>
+
 <template>
-  QuickChat
+  <div class="app">
+    <Room v-if="!state.quickChat.chatScreen" :quickChat="state.quickChat" />
+    <Chat v-else :quickChat="state.quickChat" />
+  </div>
+
 </template>
+<style scoped>
+.app {
+  background-image: url(./assets/images/quickChat.png);
+  height: 100vh;
+}
+</style>
